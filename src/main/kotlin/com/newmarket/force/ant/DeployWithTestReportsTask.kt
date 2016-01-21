780be8cac43524cefd86c38a108557f1f2ef8c86
Vladimir Almaev <almaev.vladimir@gmail.com>
2016-01-21 16:06:48 +0300

    Added support of nested batchTest elements

--- src/main/kotlin/com/newmarket/force/ant/DeployWithTestReportsTask.kt
+++ src/main/kotlin/com/newmarket/force/ant/DeployWithTestReportsTask.kt
@@ -1,7 +1,15 @@
 package com.newmarket.force.ant
 
 import com.salesforce.ant.DeployTask
+import java.util.*
+
 
 public class DeployWithTestReportsTask: DeployTask() {
+    internal final val tests = HashSet<BatchTest>()
 
+    public fun createBatchTest(): BatchTest {
+        val batch = BatchTest(getProject())
+        tests.add(batch)
+        return batch
+    }
 }
--- src/test/kotlin/com/newmarket/force/ant/DeployWithTestReportsTaskTestCase.kt
+++ src/test/kotlin/com/newmarket/force/ant/DeployWithTestReportsTaskTestCase.kt
@@ -1,5 +1,6 @@
 package com.newmarket.force.ant
 
+import org.apache.tools.ant.Project
 import org.apache.tools.ant.Task
 import org.hamcrest.MatcherAssert.assertThat;
 import org.hamcrest.Matchers.*;
@@ -9,5 +10,32 @@ import org.testng.annotations.Test
 public class DeployWithTestReportsTaskTestCase {
 
     @Test fun sut_always_shouldDeriveFromTaskClass() =
-        assertThat(DeployWithTestReportsTask(), instanceOf(Task::class.java))
+        assertThat(createSystemUnderTest(), instanceOf(Task::class.java))
+
+    @Test fun createBatchTest_always_shouldAddNewBatchTestToTests() {
+        val sut = createSystemUnderTest()
+        val actual = sut.createBatchTest()
+        assertThat(sut.tests, contains(actual))
+        assertThat(sut.getProject(), sameInstance(actual.project))
+    }
+
+    @Test fun createBatchTest_always_shouldCreatePrefixInName() {
+        assertThat(
+            "Prefix 'create' is one of the Ant's conventions for nested elements declaration. " +
+                "See the manual: http://ant.apache.org/manual/develop.html#nested-elements",
+            DeployWithTestReportsTask::createBatchTest.name,
+            startsWith("create"))
+    }
+
+    fun createSystemUnderTest(project: Project = createProject()): DeployWithTestReportsTask {
+        val sut = DeployWithTestReportsTask()
+        sut.setProject(project)
+        return sut
+    }
+
+    fun createProject(name: String = "TestProject"): Project {
+        val project = Project()
+        project.name = name
+        return project
+    }
 }
