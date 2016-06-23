46817c608f2a2820d7be053a8ef2b71875eef71f
Vladimir Almaev <almaev.vladimir@gmail.com>
2016-06-23 11:38:13 +0300

    Added javadoc for DeployTaskAdapter to explain its purpose

--- src/main/java/com/salesforce/ant/DeployTaskAdapter.java
+++ src/main/java/com/salesforce/ant/DeployTaskAdapter.java
@@ -5,6 +5,19 @@ import com.sforce.soap.metadata.MetadataConnection;
 import com.sforce.ws.ConnectionException;
 
 
+/**
+ * This class exists to being able to override methods from base class hierarchy in descendant classes.
+ * <p>
+ * Since 36.0 version of Force.com Migration tool signature of
+ * {@link SFDCMDAPIAntTask#handleResponse(MetadataConnection, StatusResult)} method was changed
+ * and it takes instance of package-private {@link SFDCMDAPIAntTask.StatusResult} interface.
+ * <p>
+ * This class:<ul>
+ * <li>have {@link com.salesforce.ant} package to being able to get access to package-private types
+ * <li>is written in Java because in Kotlin we can't access to package-private (internal in Kotlin)
+ * types and members from same package but from different .jar-file
+ * </ul>
+ */
 public class DeployTaskAdapter extends DeployTask {
     @Override
     public void handleResponse(
@@ -18,5 +31,6 @@ public class DeployTaskAdapter extends DeployTask {
         super.handleResponse(metadataConnection, response);
     }
 
-    public void handleResponse(MetadataConnection metadataConnection, AsyncResult response) { }
+    public void handleResponse(MetadataConnection metadataConnection, AsyncResult response) {
+    }
 }
