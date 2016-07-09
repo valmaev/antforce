c21ff278c62e0d17b52d424cd87ca94065840196
Vladimir Almaev <almaev.vladimir@gmail.com>
2016-07-09 22:56:34 +0300

    Added escaping of TeamCity messages.

--- src/main/kotlin/com/aquivalabs/force/ant/reporters/TeamCityReporter.kt
+++ src/main/kotlin/com/aquivalabs/force/ant/reporters/TeamCityReporter.kt
@@ -17,20 +17,20 @@ class TeamCityReporter(
 
         log("##teamcity[testSuiteStarted name='Apex']")
         result.successes.forEach {
-            log("##teamcity[testStarted name='${it.qualifiedClassName}.${it.methodName}']")
+            log("##teamcity[testStarted name='${it.qualifiedClassName.escape()}.${it.methodName.escape()}']")
             log("##teamcity[testFinished " +
-                "name='${it.qualifiedClassName}.${it.methodName}' " +
+                "name='${it.qualifiedClassName.escape()}.${it.methodName.escape()}' " +
                 "duration='${it.time / 1000}']")
         }
         result.failures.forEach {
-            log("##teamcity[testStarted name='${it.qualifiedClassName}.${it.methodName}']")
+            log("##teamcity[testStarted name='${it.qualifiedClassName.escape()}.${it.methodName.escape()}']")
             log("##teamcity[testFailed " +
-                "name='${it.qualifiedClassName}.${it.methodName}' " +
-                "message='${it.message}' " +
-                "details='${it.stackTrace}' " +
-                "type='${it.type}']")
+                "name='${it.qualifiedClassName.escape()}.${it.methodName.escape()}' " +
+                "message='${it.message.escape()}' " +
+                "details='${it.stackTrace.escape()}' " +
+                "type='${it.type.escape()}']")
             log("##teamcity[testFinished " +
-                "name='${it.qualifiedClassName}.${it.methodName}' " +
+                "name='${it.qualifiedClassName.escape()}.${it.methodName.escape()}' " +
                 "duration='${it.time / 1000}']")
         }
         log("##teamcity[testSuiteFinished name='Apex' duration='${result.totalTime / 1000}']")
@@ -44,5 +44,24 @@ class TeamCityReporter(
         log("##teamcity[buildStatisticValue key='CodeCoverageL' " +
             "value='${result.totalCoveragePercentage}']")
         log("##teamcity[blockClosed name='Apex Code Coverage Summary']")
+    }
+
+    private fun String.escape(): String {
+        val sb = StringBuilder(this.length * 2)
+        this.forEach {
+            when (it) {
+                '|' -> sb.append("||")
+                '\n' -> sb.append("|n")
+                '\r' -> sb.append("|r")
+                '\'' -> sb.append("|'")
+                '[' -> sb.append("|[")
+                ']' -> sb.append("|]")
+                '\u0085' -> sb.append("|x")
+                '\u2028' -> sb.append("|l")
+                '\u2029' -> sb.append("|p")
+                else -> sb.append(it)
+            }
+        }
+        return sb.toString()
     }
 }
--- src/test/kotlin/com/aquivalabs/force/ant/reporters/TeamCityReporterTestCase.kt
+++ src/test/kotlin/com/aquivalabs/force/ant/reporters/TeamCityReporterTestCase.kt
@@ -115,19 +115,33 @@ class TeamCityReporterTestCase {
             systemEnvironment = { env[it] },
             log = { actual.add(it) })
         val runTestsResult = createRunTestsResult(successes = arrayOf(
-            createRunTestSuccess(namespace = "foo", name = "BookTestClass", methodName = "testMethod", time = 2500.0),
-            createRunTestSuccess(namespace = "bar", name = "BarTestClass", methodName = "testAnotherMethod", time = 300.0),
-            createRunTestSuccess(name = "BookTestClass", methodName = "testMethod", time = 100.0)))
+            createRunTestSuccess(
+                namespace = "foo",
+                name = "BookTestClass",
+                methodName = "testMethod",
+                time = 2500.0),
+            createRunTestSuccess(
+                namespace = "bar",
+                name = "BarTestClass",
+                methodName = "testAnotherMethod",
+                time = 300.0),
+            createRunTestSuccess(
+                name = "BookTestClass",
+                methodName = "testMethod",
+                time = 100.0)))
 
         // Act
         sut.createReport(runTestsResult)
 
         // Assert
         runTestsResult.successes.forEach {
-            actual.contains("##teamcity[testStarted name='${it.qualifiedClassName}.${it.methodName}']")
-            actual.contains("##teamcity[testFinished " +
-                "name='${it.qualifiedClassName}.${it.methodName}' " +
-                "duration='${it.time / 1000}']")
+            assertTrue(actual.contains(
+                "##teamcity[testStarted " +
+                    "name='${it.qualifiedClassName}.${it.methodName}']"))
+            assertTrue(actual.contains(
+                "##teamcity[testFinished " +
+                    "name='${it.qualifiedClassName}.${it.methodName}' " +
+                    "duration='${it.time / 1000}']"))
         }
     }
 
@@ -153,24 +167,91 @@ class TeamCityReporterTestCase {
                 methodName = "testAnotherMethod",
                 time = 143.0,
                 type = "Exception",
-                message = "System.Exception was thrown",
-                stackTrace = "very long stacktrace")))
+                message = "System.Excepton was thrown",
+                stackTrace = "Class.TestPortalController.constructor_always_shouldSetContactAndAccount: line 16, column 1")))
 
         // Act
         sut.createReport(runTestsResult)
 
         // Assert
         runTestsResult.failures.forEach {
-            actual.contains("##teamcity[testStarted name='${it.qualifiedClassName}.${it.methodName}']")
-            actual.contains("##teamcity[testFailed " +
-                "name='${it.qualifiedClassName}.${it.methodName}' " +
-                "message='${it.message}' " +
-                "details='${it.stackTrace}' " +
-                "type='${it.type}']")
-            actual.contains("##teamcity[testFinished " +
-                "name='${it.qualifiedClassName}.${it.methodName}' " +
-                "duration='${it.time / 1000}']")
+            assertTrue(actual.contains(
+                "##teamcity[testStarted " +
+                    "name='${it.qualifiedClassName}.${it.methodName}']"))
+            assertTrue(actual.contains(
+                "##teamcity[testFailed " +
+                    "name='${it.qualifiedClassName}.${it.methodName}' " +
+                    "message='${it.message}' " +
+                    "details='${it.stackTrace}' " +
+                    "type='${it.type}']"))
+            assertTrue(actual.contains(
+                "##teamcity[testFinished " +
+                    "name='${it.qualifiedClassName}.${it.methodName}' " +
+                    "duration='${it.time / 1000}']"))
         }
+    }
+
+    @Test
+    fun createReport_ifTeamCityDetected_shouldEscapeFailureMessages() {
+        // See documentation: https://confluence.jetbrains.com/display/TCD9/Build+Script+Interaction+with+TeamCity
+
+        // Arrange
+        val env = hashMapOf("TEAMCITY_PROJECT_NAME" to "foo")
+        val actual = mutableListOf<String>()
+        val sut = createSystemUnderTest(
+            systemEnvironment = { env[it] },
+            log = { actual.add(it) })
+        val beforeEscape = "|\n\r\'[]\u0085\u2028\u2029foobar"
+        val afterEscape = "|||n|r|'|[|]|x|l|pfoobar"
+        val failure = createRunTestFailure(
+            namespace = beforeEscape,
+            name = beforeEscape,
+            methodName = beforeEscape,
+            type = beforeEscape,
+            message = beforeEscape,
+            stackTrace = beforeEscape)
+        val runTestsResult = createRunTestsResult(failures = arrayOf(failure))
+
+        // Act
+        sut.createReport(runTestsResult)
+
+        // Assert
+        assertTrue(actual.contains(
+            "##teamcity[testStarted name='$afterEscape.$afterEscape.$afterEscape']"))
+        assertTrue(actual.contains("##teamcity[testFailed " +
+            "name='$afterEscape.$afterEscape.$afterEscape' message='$afterEscape' " +
+            "details='$afterEscape' type='$afterEscape']"))
+        assertTrue(actual.contains("##teamcity[testFinished " +
+            "name='$afterEscape.$afterEscape.$afterEscape' duration='${failure.time / 1000}']"))
+    }
+
+    @Test
+    fun createReport_ifTeamCityDetected_shouldEscapeSuccessMessages() {
+        // See documentation: https://confluence.jetbrains.com/display/TCD9/Build+Script+Interaction+with+TeamCity
+
+        // Arrange
+        val env = hashMapOf("TEAMCITY_PROJECT_NAME" to "foo")
+        val actual = mutableListOf<String>()
+        val sut = createSystemUnderTest(
+            systemEnvironment = { env[it] },
+            log = { actual.add(it) })
+        val beforeEscape = "|\n\r\'[]\u0085\u2028\u2029foobar"
+        val afterEscape = "|||n|r|'|[|]|x|l|pfoobar"
+
+        val success = createRunTestSuccess(
+            namespace = beforeEscape,
+            name = beforeEscape,
+            methodName = beforeEscape)
+        val runTestsResult = createRunTestsResult(successes = arrayOf(success))
+
+        // Act
+        sut.createReport(runTestsResult)
+
+        // Assert
+        assertTrue(actual.contains(
+            "##teamcity[testStarted name='$afterEscape.$afterEscape.$afterEscape']"))
+        assertTrue(actual.contains("##teamcity[testFinished " +
+            "name='$afterEscape.$afterEscape.$afterEscape' duration='${success.time / 1000}']"))
     }
 
     fun createSystemUnderTest(
