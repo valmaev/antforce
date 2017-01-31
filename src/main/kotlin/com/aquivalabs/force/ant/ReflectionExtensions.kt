4836e7940f7bfd46650f2e2f03ab6afe1d07e6d2
Vladimir Almaev <almaev.vladimir@gmail.com>
2017-01-31 17:17:52 +0300

    Removed unused invokeDeclaredMethod function.

--- src/main/kotlin/com/aquivalabs/force/ant/ReflectionExtensions.kt
+++ src/main/kotlin/com/aquivalabs/force/ant/ReflectionExtensions.kt
@@ -14,10 +14,6 @@ internal inline fun <T : AccessibleObject, R> T.asAccessible(body: (T) -> R): R 
 }
 
 @Suppress("UNCHECKED_CAST")
-internal fun <T> Class<*>.invokeDeclaredMethod(instance: Any?, methodName: String, vararg args: Any?): T =
-    getDeclaredMethod(methodName).asAccessible { return it(instance, args) as T }
-
-@Suppress("UNCHECKED_CAST")
 internal fun <T> Class<*>.getDeclaredFieldValue(instance: Any?, fieldName: String): T =
     getDeclaredField(fieldName).asAccessible { return it[instance] as T }
 
